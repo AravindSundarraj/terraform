abcb8b26cd06926c1e22ba66bdbe2e6098bb8055
Aravind Sundarraj <aravind.ssr@gmail.com>
2023-08-13 11:02:55 -0400

    second commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+}
+
+variable "ins_name" {
+  type    = string
+  default = "demo_instance"
+}
